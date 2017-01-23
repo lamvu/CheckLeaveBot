95d8c872a8c70193c00ab845ba750889fdc8e04a
Lam Vu <Lam Vu>
2017-01-23 16:54:15 +1100

    c

--- app.js
+++ app.js
@@ -15,10 +15,24 @@ var connector = new builder.ChatConnector
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c413b2ef-382c-45bd-8ff0-f76d60e2a821?subscription-key=97e730afe44640e49c5acbca0393a6dd&q=';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
+// Add intent handlers
+dialog.matches('builtin.intent.alarm.set_alarm', builder.DialogAction.send('Creating Alarm'));
+dialog.matches('builtin.intent.alarm.delete_alarm', builder.DialogAction.send('Deleting Alarm'));
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
+
+
+
+
+/*
 // Create bot dialogs
 bot.dialog('/', function (session) {
     session.send("Hello World");
-});
+});*/
 server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html'
