4fa02cdbf3ea8669f32d4b4d4025b91ccd9f4ca0
Lam Vu <Lam Vu>
2017-02-07 17:04:10 +1100

    c

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
-// Add your requirements
 var restify = require('restify'); 
 var builder = require('botbuilder');
 var request = require('superagent');
 var ip = "https://webservices.acclimation.com.au:8543";//ECONREFUSED
-//var ip = "https://10.0.0.82:8543";//IP does not amtch certificate altnames
-//var ip = "https://10.0.0.82:8000";//error undefined
-//var ip = "https://support.acclimation.com.au:8080";//ECONREFUSED
-var query = ip + "/sap/opu/odata/GBHCM/LEAVEREQUEST;v=2/AbsenceTypeCollection(EmployeeID='',StartDate=datetime'2016-12-13T00%3A00%3A00',AbsenceTypeCode='0100')/absenceTypeTimeAccount?$select=BalancePlannedQuantity,BalanceAvailableQuantity,BalanceUsedQuantity,TimeUnitName,TimeAccountTypeName&$format=json";
-
+var leaveQuery = ip + "/sap/opu/odata/GBHCM/LEAVEREQUEST;v=2/AbsenceTypeCollection(EmployeeID='',StartDate=datetime'2016-12-13T00%3A00%3A00',AbsenceTypeCode='0100')/absenceTypeTimeAccount?$select=BalancePlannedQuantity,BalanceAvailableQuantity,BalanceUsedQuantity,TimeUnitName,TimeAccountTypeName&$format=json";
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
+server.listen(process.env.PORT || 3000, function() {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
-var searchHotels = function (uname, pword) {
+var checkLeave = function (uname, pword) {
     return new Promise(function(resolve, reject) {
-        request.get(query).auth(uname, pword).end(function(err, res){
+        request.get(leaveQuery).auth(uname, pword).end(function(err, res){
             if (err || !res.ok) {
-                return reject('ERROR')
+                return reject(err.status)
             } else {
-                //var result = JSON.stringify(res.body);
                 resolve(res.body);
             }
         });
     });
 }
-
-// Create chat bot
+var checkSale = function (uname, pword) {
+    return new Promise(function(resolve, reject) {
+        request.get(leaveQuery).auth(uname, pword).end(function(err, res){
+            if (err || !res.ok) {
+                return reject(err.status)
+            } else {
+                resolve(res.body);
+            }
+        });
+    });
+}
+var checkPurchase = function (uname, pword) {
+    return new Promise(function(resolve, reject) {
+        request.get(leaveQuery).auth(uname, pword).end(function(err, res){
+            if (err || !res.ok) {
+                return reject(err.status)
+            } else {
+                resolve(res.body);
+            }
+        });
+    });
+}
 var connector = new builder.ChatConnector
 ({ appId: '4287d7b4-aa06-4800-97a6-d842ef67733d', appPassword: 'yS8VVU9MD42EqwbR0LdCv9j' }); 
 var bot = new builder.UniversalBot(connector);
-var bot = new builder.UniversalBot(connector, function (session) {
-    var address = session.message.address;
-    userStore.push(address);
-
-    // end current dialog
-    session.endDialog('I can answer your leave queries ! It will start in a few seconds...');
-});
-
 server.post('/api/messages', connector.listen());
-
-// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
 var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c413b2ef-382c-45bd-8ff0-f76d60e2a821?subscription-key=97e730afe44640e49c5acbca0393a6dd&q=';
 var recognizer = new builder.LuisRecognizer(model);
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
@@ -58,67 +60,96 @@ bot.use({
      }
 });
 
-setInterval(function () {
-    var newAddresses = userStore.splice(0);
-    newAddresses.forEach(function (address) {
-
-        console.log('Starting session for user:', address);
-
-        // new conversation address, copy without conversationId
-        var newConversationAddress = Object.assign({}, address);
-        delete newConversationAddress.conversation;
-
-        // start survey dialog
-        bot.beginDialog(newConversationAddress, 'survey', null, (err) => {
-            if (err) {
-                // error ocurred while starting new conversation. Channel not supported?
-                bot.send(new builder.Message()
-                    .text('This channel does not support this operation: ' + err.message)
-                    .address(address));
-            }
-        });
-
-    });
-}, 5000);
-
-bot.dialog('survey', [
+var uname = 'vsdtechno';
+var pword = 'welcome1';
+dialog.matchesAny([/hi/i, /hello/i, /good/i], [
     function (session) {
-        builder.Prompts.text(session, 'Hello... What\'s your name?');
+        builder.Prompts.text(session, "Hello. What is your name ?");
     },
     function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.text(session, 'Hi ' + results.response + ', What is username to connect to database ?');
+        session.send("Nice to meet you, %s", results.response);
+        builder.Prompts.choice(session, 'What do you want to do to day ?', ['Leave Application', 'Sales Order', 'Purchase Order'], {retryPrompt:'Sorry I dont understand. Please answer again:'});
+    },
+    function (session, results) {
+        session.userData.choice = results.response.entity.toLowerCase() || results.response.entity;
+        if(session.userData.choice == '1' || session.userData.choice.indexOf('leave') != -1) {
+            builder.Prompts.text(session, 'What is username to connect to database ?');
+        } else if(session.userData.choice == '2' || session.userData.choice.indexOf('sale') != -1) {
+            session.send("Sorry I cant answer you now. This function is not implemented yet");
+            session.endDialog();
+
+        } else if(session.userData.choice == '3' || session.userData.choice.indexOf('purchase') != -1) {
+            session.send("Sorry I cant answer you now. This function is not implemented yet");
+            session.endDialog();
+        }
     },
     function (session, results) {
         session.userData.uname = results.response;
-        builder.Prompts.text(session, 'Hi ' + results.response + ', What is password to connect to database ?');
-        //builder.Prompts.choice(session, 'What language do you code Node using? ', ['JavaScript', 'CoffeeScript', 'TypeScript']);
+        builder.Prompts.text(session, 'What is password to connect to database ?');
     },
     function (session, results) {
-        session.userData.pword = results.response.entity;
-        searchHotels(session.userData.uname, session.userData.pword).then((result) => {
+        session.userData.pword = results.response;
+        interact(session);
+    }
+]);
+var interact = function (session) {
+    if(session.userData.choice == '1' || session.userData.choice.indexOf('leave') != -1) {
+
+        checkLeave(session.userData.uname, session.userData.pword).then((result) => {
             var res = result.d.results[0];
             var typeName = res.TimeAccountTypeName;
             var unitName = res.TimeUnitName;
             var uQua = res.BalanceUsedQuantity;
             var pQua = res.BalancePlannedQuantity;
             var aQua = res.BalanceAvailableQuantity;
-            var str = session.userData.name + ", you have used " + uQua + " days, planned " + pQua + " days, had " + aQua + " days leave";
+            var str = "You have used " + uQua + " days, planned " + pQua + " days, had " + aQua + " days leave";
             session.send(str);
             session.endDialog();
         }).catch((err) => {
-            session.send("error: " + err);
+            if(err == 401) session.send("Unauthorized. Your username and password may be incorrect.");
+            else session.send("Status is " + err + ". Unknown error. Server may have problem.");
             session.endDialog();
         });
-        /*
-        session.endDialog('Got it... ' + session.userData.name +
-            ' you\'ve been programming for ' + session.userData.coding +
-            ' years and use ' + session.userData.language + '.');*/
+
+    } else if(session.userData.choice == '2' || session.userData.choice.indexOf('sale') != -1) {
+        session.send("This function is not implemented yet");
+        session.endDialog();
+
+    } else if(session.userData.choice == '3' || session.userData.choice.indexOf('purchase') != -1) {
+        session.send("This function is not implemented yet");
+        session.endDialog();
+    }
+}
+dialog.matches(/leave/i, [
+    function (session) {
+        builder.Prompts.text(session, 'What is username to connect to database ?');
+    },
+    function (session, results) {
+        session.userData.uname = results.response;
+        builder.Prompts.text(session, 'What is password to connect to database ?');
+        //builder.Prompts.choice(session, 'What language do you code Node using? ', ['JavaScript', 'CoffeeScript', 'TypeScript']);
+    },
+    function (session, results) {
+        session.userData.pword = results.response;
+        console.log(session.userData.uname);
+        console.log(session.userData.pword);
+        checkLeave(session.userData.uname, session.userData.pword).then((result) => {
+            var res = result.d.results[0];
+            var typeName = res.TimeAccountTypeName;
+            var unitName = res.TimeUnitName;
+            var uQua = res.BalanceUsedQuantity;
+            var pQua = res.BalancePlannedQuantity;
+            var aQua = res.BalanceAvailableQuantity;
+            var str = "You have used " + uQua + " days, planned " + pQua + " days, had " + aQua + " days leave";
+            session.send(str);
+            session.endDialog();
+        }).catch((err) => {
+            if(err == 401) session.send("Unauthorized. Your username and password may be incorrect.");
+            else session.send("Status is " + err + ". Unknown error. Server may have problem.");
+            session.endDialog();
+        });
     }
 ]);
-
-
-
 dialog.onDefault(builder.DialogAction.send("I didn't understand. I can check leave for you."));
 server.get('/', restify.serveStatic({
  directory: __dirname,
--- app1.js
+++ app1.js
@@ -1,39 +1,27 @@
-// Add your requirements
 var restify = require('restify'); 
 var builder = require('botbuilder');
 var request = require('superagent');
 var ip = "https://webservices.acclimation.com.au:8543";//ECONREFUSED
-//var ip = "https://10.0.0.82:8543";//IP does not amtch certificate altnames
-//var ip = "https://10.0.0.82:8000";//error undefined
-//var ip = "https://support.acclimation.com.au:8080";//ECONREFUSED
 var query = ip + "/sap/opu/odata/GBHCM/LEAVEREQUEST;v=2/AbsenceTypeCollection(EmployeeID='',StartDate=datetime'2016-12-13T00%3A00%3A00',AbsenceTypeCode='0100')/absenceTypeTimeAccount?$select=BalancePlannedQuantity,BalanceAvailableQuantity,BalanceUsedQuantity,TimeUnitName,TimeAccountTypeName&$format=json";
-
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
+server.listen(process.env.PORT || 3000, function() {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
-var searchHotels = function (destination) {
+var checkLeave = function (uname, pword) {
     return new Promise(function(resolve, reject) {
-        request.get(query).auth('vsdtechno', 'welcome1').end(function(err, res){
+        request.get(query).auth(uname, pword).end(function(err, res){
             if (err || !res.ok) {
-                return reject('ERROR')
+                return reject(err.status)
             } else {
-                //var result = JSON.stringify(res.body);
                 resolve(res.body);
             }
         });
     });
 }
-
-// Create chat bot
 var connector = new builder.ChatConnector
 ({ appId: '4287d7b4-aa06-4800-97a6-d842ef67733d', appPassword: 'yS8VVU9MD42EqwbR0LdCv9j' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-
-// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
 var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c413b2ef-382c-45bd-8ff0-f76d60e2a821?subscription-key=97e730afe44640e49c5acbca0393a6dd&q=';
 var recognizer = new builder.LuisRecognizer(model);
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
@@ -49,7 +37,9 @@ bot.use({
           next(); 
      }
 });
-// Add intent handlers
+
+var uname = 'vsdtechno';
+var pword = 'welcome1';
 dialog.matchesAny([/hi/i, /hello/i, /good/i], [
     function (session) {
         builder.Prompts.text(session, "Hello. What is your name ?");
@@ -58,29 +48,36 @@ dialog.matchesAny([/hi/i, /hello/i, /good/i], [
         session.send("Nice to meet you, %s", results.response);
     }
 ]);
-
-dialog.matches(/^version/i, function (session) {
-    session.send('Bot version 1.2');
-});
-
-dialog.matches(/leave/i, function (session) {
-    var destination = "1";
-    searchHotels(destination).then((result) => {
-        var res = result.d.results[0];
-        var typeName = res.TimeAccountTypeName;
-        var unitName = res.TimeUnitName;
-        var uQua = res.BalanceUsedQuantity;
-        var pQua = res.BalancePlannedQuantity;
-        var aQua = res.BalanceAvailableQuantity;
-        var str = "You have used " + uQua + " days, planned " + pQua + " days, had " + aQua + " days leave";
-        session.send(str);
-        session.endDialog();
-    }).catch((err) => {
-        session.send("error: " + err);
-        session.endDialog();
-    });
-});
-
+dialog.matches(/leave/i, [
+    function (session) {
+        builder.Prompts.text(session, 'What is username to connect to database ?');
+    },
+    function (session, results) {
+        session.userData.uname = results.response;
+        builder.Prompts.text(session, 'What is password to connect to database ?');
+        //builder.Prompts.choice(session, 'What language do you code Node using? ', ['JavaScript', 'CoffeeScript', 'TypeScript']);
+    },
+    function (session, results) {
+        session.userData.pword = results.response;
+        console.log(session.userData.uname);
+        console.log(session.userData.pword);
+        checkLeave(session.userData.uname, session.userData.pword).then((result) => {
+            var res = result.d.results[0];
+            var typeName = res.TimeAccountTypeName;
+            var unitName = res.TimeUnitName;
+            var uQua = res.BalanceUsedQuantity;
+            var pQua = res.BalancePlannedQuantity;
+            var aQua = res.BalanceAvailableQuantity;
+            var str = "You have used " + uQua + " days, planned " + pQua + " days, had " + aQua + " days leave";
+            session.send(str);
+            session.endDialog();
+        }).catch((err) => {
+            if(err == 401) session.send("Unauthorized. Your username and password may be incorrect.");
+            else session.send("Status is " + err + ". Unknown error. Server may have problem.");
+            session.endDialog();
+        });
+    }
+]);
 dialog.onDefault(builder.DialogAction.send("I didn't understand. I can check leave for you."));
 server.get('/', restify.serveStatic({
  directory: __dirname,
